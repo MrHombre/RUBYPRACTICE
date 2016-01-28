d334c25b9fbd0c32cc0074ede4c464746222be4a
James Lett <jlett89@gmail.com>
2016-01-27 21:47:48 -0800

    setting the size of our invaders there init speed and how they will be represented

--- SpaceInv.rb
+++ SpaceInv.rb
@@ -11,11 +11,24 @@ end
  invaders_per_row = 10
  rows = 5
 
- @monsters = []
+ @invaders = []
  @bullets = []
 
  # Now the invaders are going to be a bit big so that they'll fit on the screen
  invaders_girth = self.height / invaders_per_row / 2.4
  invaders_girth = invaders_girth * 2
 
- 
+ # Invaders move slow..you know at the start
+ invaders_movement = self.width / 600
+
+ # Lets center them!
+ start_x = (self.width - invaders_space * invaders_per_row) / 2
+
+ # Each of the invaders will be represented by a square...but it can be changed
+ rows.times do |y|
+   invaders_per_row.times do |x|
+     ix = x * invaders_space + start_x
+     iy = y * invaders_space + invaders_girth * 2
+     @invaders << rect(ix, iy, invaders_girth, :fill => white)
+   end
+ end
