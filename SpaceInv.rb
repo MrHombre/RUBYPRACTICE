319938b4d7e2342eec9827edd71b8e13d498df69
James Lett <jlett89@gmail.com>
2016-01-27 21:36:11 -0800

    New file and init set for game Space Invaders

---
+++ SpaceInv.rb
@@ -0,0 +1,8 @@
+# Sapce Invaders Clone Thing :B
+
+# This creates a new window for the game
+# Oh and were using Shoes GUI I'll put a link in the README
+
+Shoes.app :width => 600, :height => 400, :resizable => false do
+  background :black
+end
