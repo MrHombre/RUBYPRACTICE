d9e689661b86c92e277fbedf60d8d7c089ef3d5e
James Lett <jlett89@gmail.com>
2016-01-27 22:04:13 -0800

    set the invaders to only more left and right fix a bug with the wall and what happend if invaders win

--- SpaceInv.rb
+++ SpaceInv.rb
@@ -15,8 +15,8 @@ end
  @bullets = []
 
  # Now the invaders are going to be a bit big so that they'll fit on the screen
- invaders_girth = self.height / invaders_per_row / 2.4
- invaders_girth = invaders_girth * 2
+ invader_girth = self.height / invaders_per_row / 2.4
+ invader_girth = invader_girth * 2
 
  # Invaders move slow..you know at the start
  invaders_movement = self.width / 600
@@ -28,14 +28,14 @@ end
  rows.times do |y|
    invaders_per_row.times do |x|
      ix = x * invaders_space + start_x
-     iy = y * invaders_space + invaders_girth * 2
-     @invaders << rect(ix, iy, invaders_girth, :fill => white)
+     iy = y * invaders_space + invader_girth * 2
+     @invaders << rect(ix, iy, invader_girth, :fill => white)
    end
  end
 
  # The player is also a square to keep it simple
- px = (self.width - invaders_girth) / 2
- @player = rect px, self.height - invaders_girth * 2, invaders_girth, :fill => blue
+ px = (self.width - invader_girth) / 2
+ @player = rect px, self.height - invader_girth * 2, invader_girth, :fill => blue
 
  # The bullets are slim ovals
  @bullet = oval 0, 0, 4, 7, :fill => yellow
@@ -49,9 +49,29 @@ end
    i = @invaders[rand(@invaders.size)]
 
    # We center the bullet
-   bx = i.left + invaders_girth / 2 + 5
-   by = i.top + invaders_girth - 5
+   bx = i.left + invader_girth / 2 + 5
+   by = i.top + invader_girth - 5
 
    # BOOM! FIRE!!!
-   @bullets << oval(bc, by, 10 :fill => green) 
+   @bullets << oval(bc, by, 10 :fill => green)
+ end
+
+ @anim = animate 200  do
+   change_directions = false
+
+   # We want the invaders to only go left and right
+   @invaders.each do |i|
+     i.move i.left + invaders_movement, i.top
+
+     # Hitting a wall blowing up the game?! Here a fix
+     if i.left + invader_girth + invaders_movement > self.width || i.left + invaders_movement < 0
+       change_directions = true
+     end
+
+     # If the invader reaches the bottom
+     if i.top + invader_girth > @player.top
+       @anim.stop && alert("You lose!")
+       break
+     end
+   end
  end
