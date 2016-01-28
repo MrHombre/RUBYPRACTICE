cf5bd392e258c33fa12448200b80e1c83a31a642
James Lett <jlett89@gmail.com>
2016-01-27 21:41:42 -0800

    Change the name from monster they're invaders damnit!

--- SpaceInv.rb
+++ SpaceInv.rb
@@ -7,13 +7,15 @@ Shoes.app :width => 600, :height => 400, :resizable => false do
   background :black
 end
 
- # Creating some monster
- monsters_per_row = 10
+ # Creating some invaders
+ invaders_per_row = 10
  rows = 5
 
  @monsters = []
  @bullets = []
 
- # Now the monster are going to be a bit big so that they'll fit on the screen
- monster_girth = self.height / monsters_per_row / 2.4
- monster_girth = monster_girth * 2
+ # Now the invaders are going to be a bit big so that they'll fit on the screen
+ invaders_girth = self.height / invaders_per_row / 2.4
+ invaders_girth = invaders_girth * 2
+
+ 
