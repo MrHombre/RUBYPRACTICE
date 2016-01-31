8a9bc01611687ee6d33b8884aebd4c9a1ed53103
James Lett <jlett89@gmail.com>
2016-01-30 18:52:04 -0800

    Created a line store and colur . As the mouse moves around and the bubbles grow this color linesman should show up

--- Bubbles.rb
+++ Bubbles.rb
@@ -1,4 +1,4 @@
-# Grwoing Bubbles when the mous move around the app window
+# Grwoing Bubbles when the mouse move around the app window
 
 # Array of x,y coordinates for bubbles
 bubbles = [[0,0]] * 30
@@ -9,5 +9,14 @@ Shoes.app :width => 537, :height => 500 do
   animate(24) do
     bubbles.shift
     bubbles << self.mouse[1,2]
+
+    # This is for the pink-ish linescan which can be changed
+    clear do
+      (500/5).times do |i|
+        strokewidth 2
+        stroke rgb(1.0. 0.5, 1.0, 1.0)
+        line 0, i * 5, 537, i * 5
+      end
+    end
   end
 end
