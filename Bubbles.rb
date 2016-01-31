508af6456009f92097c61bc3b79df06e6b0d3c16
James Lett <jlett89@gmail.com>
2016-01-30 19:03:38 -0800

    Revert "add animate to bubbles"
    This reverts commit 5444903bec21c0bec128071e089419702b31c1e9.

--- Bubbles.rb
+++ Bubbles.rb
@@ -5,9 +5,5 @@ bubbles = [[0,0]] * 30
 
 # Bubbles App
 Shoes.app :width => 537, :height => 500 do
-  # This is going to be slow about 24 frames/ second
-  animate(24) do
-    bubbles.shift
-    bubbles << self.mouse[1,2]
-  end
+  
 end
