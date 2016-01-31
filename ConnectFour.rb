f6db6619c3066be6ccf0cb037538fea61e4df1b6
James Lett <jlett89@gmail.com>
2016-01-30 18:12:20 -0800

    Set up the board

--- ConnectFour.rb
+++ ConnectFour.rb
@@ -1,5 +1,8 @@
 # Human vs Human Connect Four
 
 Shoes.app :width => 640 :height => 640 :resizable => false do
-  
+  # Build an 8x8 board as a set of ovals
+  @board = Array.new(8) do |x|
+    oval(x*80, y*80, 80 :fill => white)
+  end
 end
