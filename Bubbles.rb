a642940ad9ce365a5fb5badcbdcdfcffce3fc3e5
James Lett <jlett89@gmail.com>
2016-01-30 18:46:25 -0800

    Setting app window for bubbles

--- Bubbles.rb
+++ Bubbles.rb
@@ -1,1 +1,9 @@
 # Grwoing Bubbles when the mous move around the app window
+
+# Array of x,y coordinates for bubbles
+bubbles = [[0,0]] * 30
+
+# Bubbles App
+Shoes.app :width => 537, :height => 500 do
+  
+end
