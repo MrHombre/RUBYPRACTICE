129aa189d3cee978207410999407d9967d138185
James Lett <jlett89@gmail.com>
2016-02-01 12:20:30 -0800

    Set test and failing

--- WordFrequencies/spec.rb
+++ WordFrequencies/spec.rb
@@ -1,2 +1,10 @@
 require 'minitest/autorun'
 require_relative 'word_frequencies'
+
+describe 'f' do
+  it 'should return each word and the number of times, each word appears in the string' do
+    expect(f("To be, or not to be.")).to eq(['to, 2'], ['be, 2'], ['or, 1'], ['not, 1'])
+
+    expect(f("Hello World!")).to eq(['hello, 1'], ['world, 1'])
+  end
+end
