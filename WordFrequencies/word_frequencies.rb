b341162e7c8fad1d39d6a35821f38b656e45f459
James Lett <jlett89@gmail.com>
2016-02-02 11:41:55 -0800

    Add a comment

--- WordFrequencies/word_frequencies.rb
+++ WordFrequencies/word_frequencies.rb
@@ -1,9 +1,19 @@
+#Adding a comment
+
 def f(p)
   words = p.downcase.split(/\W+/)
+
+
   new_array = []
 
+
+
   words.each do |word|
-    new_array << [word, words.count{|x| x == word}]
+    new_array << [word, words.count{|x| x == word}]  #Creating a loop
+                                                     #Takes the new_array addes our 'unique words'
+                                                     #Counts the frequency of the words.
   end
+
+
   new_array.uniq
 end
